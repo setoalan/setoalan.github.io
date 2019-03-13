c485281776daab120ca51973bd8ceeaa1b7fbcf9
Alan Seto <alantseto@gmail.com>
2019-03-13 14:36:10 -0500

    More refactor and add backgound image

---
+++ alanseto.dev/src/components/App/App.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+import './App.scss';
+
+export class App extends Component {
+  render() {
+    return (
+      <div className="App">
+      </div>
+    );
+  }
+}
